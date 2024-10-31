6d99bf67e1f3db7fdc66ce9937aa1b441bd7f45e
Samuel-Omobuwa <samuelomobuwa1@gmail.com>
2024-10-31 02:44:10 +0100

    week_1_assignment

---
+++ user_input.py
@@ -0,0 +1,9 @@
+name = input("Please enter your name")
+
+age = input("Please fill your age")
+
+location = input("Kindly update your location")
+
+# Printing a personalized message
+print(f"Hello {name}, you are {age} years old and lives in {location}")
+
